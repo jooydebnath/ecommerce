207c0f61e6ea8ff274dbe1794519dbf09bdee238
Joy Debnath <joy91740@gmail.com>
2021-10-08 12:02:18 +0600

    store cart page create and store app create

---
+++ store/views.py
@@ -0,0 +1,16 @@
+from django.shortcuts import render
+
+
+def store(request):
+    context = {}
+    return render(request, 'store.html', context)
+
+
+def cart(request):
+    context = {}
+    return render(request, 'cart.html', context)
+
+
+def checkout(request):
+    context = {}
+    return render(request, 'checkout.html', context)
